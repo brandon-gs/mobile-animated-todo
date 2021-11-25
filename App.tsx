71f7f9cd6d75476447a262d759aeccbfcfcb1cb4
BrandonGS <brandongs180@gmail.com>
2021-11-25 14:22:31 -0600

    Integrate react-navigation and drawer

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import AppContainer from './src/components/app-container'
+import Navigator from './src'
 import MainScreen from './src/screens/main'
 
 export default function App() {
   return (
     <AppContainer>
-      <MainScreen />
+      <Navigator />
     </AppContainer>
   )
 }
---
+++ src/screens/about-screen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Text, VStack } from 'native-base'
+
+function AboutScreen() {
+  return (
+    <VStack flex={1}>
+      <Box flex={1}>
+        <Text>About</Text>
+      </Box>
+    </VStack>
+  )
+}
+
+export default AboutScreen
