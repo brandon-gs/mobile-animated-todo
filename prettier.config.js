af6c833ca1a3b318a354195db3b5a7200fe58e93
BrandonGS <brandongs180@gmail.com>
2021-11-24 23:48:29 -0600

    Initialize project

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: "avoid",
+  singleQuote: true,
+  backedSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingComa: "none",
+};
+
+module.exports = options;
