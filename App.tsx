adcf6201f2236d7e86982783d664829eb7e15c9d
BrandonGS <brandongs180@gmail.com>
2021-11-24 23:57:06 -0600

    Prepare to use native base

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import AppContainer from './src/components/app-container'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <AppContainer>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </AppContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +21,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
